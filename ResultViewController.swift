90820b6b6ee1bfe8dc274c64647fbd35b8c55c99
福園康弘 <moze478@yahoo.co.jp>
2017-08-16 06:09:38 +0900

    Lesson4の課題

---
+++ ResultViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 福園康弘 on 2017/08/16.
+//  Copyright © 2017年 yasuhiro.fukuzono. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var result_label: UILabel!
+    
+    var name:String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        result_label.text = "こんにちは、 \(name) さん"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var input_textfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // segueから遷移先のResultViewControllerを取得する
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
+        resultViewController.name = input_textfield.text!
+    }
 
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    
+    }
 }
 
